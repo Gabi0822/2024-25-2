ba3d1fe52c41d6992bd22bfc62480db105fbb648
Consuegra Gabor <pgox1h@inf.elte.hu>
2025-03-06 16:34:14 +0100

    #3 Blade Template-k és Admin-hoz tartozó jelszó

--- gyak-7-csoport/database/seeders/DatabaseSeeder.php
+++ gyak-7-csoport/database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,6 +19,7 @@ class DatabaseSeeder extends Seeder
             'name' => 'Admin',
             'email' => 'admin@admin.hu',
             'admin' => true,
+            'password' => Hash::make('admin'),
         ]);
 
         User::factory(10)->create();
