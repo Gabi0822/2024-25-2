8a7e75dfb6a6c57339fad9cdf049587cf946a490
Consuegra Gabor <pgox1h@inf.elte.hu>
2025-04-04 15:47:53 +0200

    #7 Laravel Sanctum, hitelesítés, regisztráció token alapon.

---
+++ gyak-6-csoport/app/Http/Controllers/ApiAuthController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+use Illuminate\Support\Facades\Validator;
+ use Illuminate\Validation\Rules\Password;
+
+class ApiAuthController extends Controller
+{
+    function register(Request $request) {
+        $validator = Validator::make(
+            $request->all(),
+            [
+                'name' => 'required|string',
+                'email' => 'required|string|email|unique:users,email',
+                'password' => ['required', 'string', Password::min(8)->letters()->mixedCase()->numbers()],
+            ],
+            [
+                'required' => ':attribute mező megadása kötelező!',
+                'string' => ':attribute mezo csak szoveges lehet!',
+                'email' => ':attribute mezo csak helyesen formazott email lehet!',
+                'unique' => ':attribute cim mar foglalt!',
+            ],
+            [
+                'name' => 'A nev',
+                'email' => 'Az email',
+                'password' => 'A jelszo',
+            ]
+            );
+            if($validator->fails()) {
+                return response()->json([
+                    'error' => $validator->messages(),
+                ], 400);
+            }
+
+            $validated = $validator->validated();
+
+            $user = User::create($validated);
+
+            $token = $user->createToken('auth', $user->admin ? ['ticket:admin'] : ['ticket:user']);
+
+            return response()->json([
+                'token' => $token->plainTextToken,
+                'raw' => $token,
+            ], 201);
+    }
+}
--- gyak-6-csoport/bootstrap/app.php
+++ gyak-6-csoport/bootstrap/app.php
@@ -3,15 +3,18 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use App\Http\Middleware\ForceJSONResponseAPIMiddleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
+        apiPrefix: 'api',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->api(prepend: [ForceJSONResponseApiMiddleware::class]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
